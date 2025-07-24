9ea4c79821875908ed5cc642120eadf1268c43d3
jjj <jrnw1706@gmail.com>
2025-07-24 21:32:28 +0200

    Create Plat_FloatTime.cpp

---
+++ Amalgam/src/Hooks/Plat_FloatTime.cpp
@@ -0,0 +1,6 @@
+#include "../SDK/SDK.h"
+
+MAKE_HOOK(Plat_FloatTime, U::Memory.GetModuleExport<uintptr_t>("tier0.dll", "Plat_FloatTime"), double)
+{
+	return CALL_ORIGINAL() + G::PlatFloatTimeAdd;
+}
