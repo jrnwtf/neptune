685f2c3776747e12c3286703421d26a940d5cf65
jjj <jrnw1706@gmail.com>
2025-07-24 19:47:07 +0200

    Update Memory.cpp

--- Amalgam/src/Utils/Memory/Memory.cpp
+++ Amalgam/src/Utils/Memory/Memory.cpp
@@ -119,3 +119,188 @@ std::string CMemory::GetModuleOffset(uintptr_t uAddress)
 
 	return std::format("{}+{:#x}", buffer, uAddress - uBase);
 }
+
+HMODULE CMemory::GetModuleHandleSafe(const std::string& pszModuleName)
+{
+	HMODULE hmModuleHandle = NULL;
+
+	do
+	{
+		if (pszModuleName.empty())
+			hmModuleHandle = GetModuleHandleA(NULL);
+		else
+			hmModuleHandle = GetModuleHandleA(pszModuleName.c_str());
+
+		std::this_thread::sleep_for(std::chrono::microseconds(100));
+	} while (hmModuleHandle == NULL);
+
+	return hmModuleHandle;
+}
+
+std::pair<DWORD, DWORD> CMemory::GetModuleSize(const std::string& pszModuleName)
+{
+	HMODULE hmModule = GetModuleHandleSafe(pszModuleName);
+
+	PIMAGE_DOS_HEADER pDOSHeader = (PIMAGE_DOS_HEADER)hmModule;
+	PIMAGE_NT_HEADERS pNTHeaders = (PIMAGE_NT_HEADERS)(((DWORD)hmModule) + pDOSHeader->e_lfanew);
+
+	DWORD dwAddress = ((DWORD)hmModule) + pNTHeaders->OptionalHeader.BaseOfCode;
+	DWORD dwLength = ((DWORD)hmModule) + pNTHeaders->OptionalHeader.SizeOfCode;
+
+	return { dwAddress, dwLength };
+}
+
+PVOID CMemory::GetVirtualFunction(PVOID inst, DWORD index)
+{
+	if (inst == nullptr)
+		return nullptr;
+
+	PDWORD table = *(reinterpret_cast<PDWORD*>(inst));
+
+	if (table == nullptr)
+		return nullptr;
+
+	return reinterpret_cast<PVOID>(table[index]);
+}
+
+DWORD CMemory::CalcInstAddress(DWORD inst)
+{
+	if (!inst)
+		return 0;
+
+	switch (*reinterpret_cast<BYTE*>(inst))
+	{
+	// call ?? ?? ?? ??
+	case 0xE8:
+	{
+		DWORD target = *reinterpret_cast<DWORD*>(inst + 1);
+		DWORD next = inst + 5;
+		return target + next;
+	}
+
+	// jmp ?? ?? ?? ??
+	case 0xE9:
+	{
+		DWORD target = *reinterpret_cast<DWORD*>(inst + 1);
+		DWORD next = inst + 5;
+		return target + next;
+	}
+
+	// mov ?? ?? ?? ?? ??
+	case 0x8B:
+	{
+		switch (*reinterpret_cast<BYTE*>(inst + 1))
+		{
+		// mov ?? ?? ?? ?? ??
+		case 0x0D:
+		{
+			DWORD target = *reinterpret_cast<DWORD*>(inst + 2);
+			return target;
+		}
+		}
+
+		break;
+	}
+
+	// mov ?? ?? ?? ??
+	case 0xB9:
+	{
+		DWORD target = *reinterpret_cast<DWORD*>(inst + 1);
+		return target;
+	}
+	}
+
+	return inst;
+}
+
+BOOL CMemory::RemoveEntryList(PLIST_ENTRY Entry)
+{
+	PLIST_ENTRY Blink, Flink;
+
+	Flink = Entry->Flink; Blink = Entry->Blink;
+	Blink->Flink = Flink; Flink->Blink = Blink;
+
+	return (BOOL)(Flink == Blink);
+}
+
+BOOL CMemory::ClearPEHeader(HINSTANCE hModule)
+{
+	PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)hModule;
+
+	if (pDosHeader->e_magic == IMAGE_DOS_SIGNATURE)
+	{
+		PIMAGE_NT_HEADERS pNtHeader = (PIMAGE_NT_HEADERS)((DWORD)pDosHeader + (DWORD)pDosHeader->e_lfanew);
+
+		if (pNtHeader->Signature == IMAGE_NT_SIGNATURE)
+		{
+			DWORD dwOld, dwOld2, dwSize = pNtHeader->OptionalHeader.SizeOfHeaders;
+
+			if (VirtualProtect((LPVOID)pDosHeader, dwSize, PAGE_EXECUTE_READWRITE, &dwOld))
+			{
+				ZeroMemory(pDosHeader, dwSize);
+				VirtualProtect((LPVOID)pDosHeader, dwSize, dwOld, &dwOld2);
+				return TRUE;
+			}
+		}
+	}
+
+	return FALSE;
+}
+
+bool CMemory::RemoveHeader(HINSTANCE hinstDLL)
+{
+	DWORD ERSize = 0;
+	DWORD Protect = 0;
+	DWORD dwStartOffset = (DWORD)hinstDLL;
+
+	IMAGE_DOS_HEADER* pDosHeader = (PIMAGE_DOS_HEADER)dwStartOffset;
+	IMAGE_NT_HEADERS* pNtHeader = (PIMAGE_NT_HEADERS)(dwStartOffset + pDosHeader->e_lfanew);
+
+	ERSize = sizeof(IMAGE_NT_HEADERS);
+
+	if (VirtualProtect(pDosHeader, ERSize, PAGE_EXECUTE_READWRITE, &Protect))
+	{
+		for (DWORD i = 0; i < ERSize - 1; i++)
+			*(PBYTE)((DWORD)pDosHeader + i) = 0x00;
+	}
+	else
+		return false;
+
+	VirtualProtect(pDosHeader, ERSize, Protect, 0);
+
+	ERSize = sizeof(IMAGE_DOS_HEADER);
+
+	if ((pNtHeader != 0) && VirtualProtect(pNtHeader, ERSize, PAGE_EXECUTE_READWRITE, &Protect))
+	{
+		for (DWORD i = 0; i < ERSize - 1; i++)
+			*(PBYTE)((DWORD)pNtHeader + i) = 0x00;
+	}
+	else
+		return false;
+
+	VirtualProtect(pNtHeader, ERSize, Protect, 0);
+	return true;
+}
+
+bool CMemory::HideThread(HANDLE hThread)
+{
+	typedef NTSTATUS(NTAPI* pNtSetInformationThread)(HANDLE, UINT, PVOID, ULONG);
+	NTSTATUS Status;
+
+	pNtSetInformationThread NtSIT = (pNtSetInformationThread)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtSetInformationThread");
+
+	if (NtSIT == NULL)
+	{
+		return false;
+	}
+
+	if (hThread == NULL)
+		Status = NtSIT(GetCurrentThread(), 0x11, 0, 0);
+	else
+		Status = NtSIT(hThread, 0x11, 0, 0);
+
+	if (Status != 0x00000000)
+		return false;
+	else
+		return true;
+}
