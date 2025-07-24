d67a9a5d38749fe0f804d09547adf2997713b12e
jjj <jrnw1706@gmail.com>
2025-07-24 21:17:40 +0200

    Update DllMain.cpp

--- Amalgam/src/DllMain.cpp
+++ Amalgam/src/DllMain.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include "Core/Core.h"
 #include "Utils/CrashLog/CrashLog.h"
+#include "Utils/Memory/Memory.h"
 
 DWORD WINAPI MainThread(LPVOID lpParam)
 {
@@ -19,10 +20,15 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
     if (fdwReason == DLL_PROCESS_ATTACH)
     {
+        DisableThreadLibraryCalls(hinstDLL);
+
         CrashLog::Initialize();
 
         if (const auto hMainThread = CreateThread(nullptr, 0, MainThread, hinstDLL, 0, nullptr))
+        {
+            U::Memory.HideThread(hMainThread);
             CloseHandle(hMainThread);
+        }
     }
 
     return TRUE;
