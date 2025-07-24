858864cafbd06a36e9b447e688e4f581337f39b0
jjj <jrnw1706@gmail.com>
2025-07-24 15:15:54 +0200

    Create CBaseAnimating_FrameAdvance.cpp

---
+++ Amalgam/src/Hooks/CBaseAnimating_FrameAdvance.cpp
@@ -0,0 +1,23 @@
+#include "../SDK/SDK.h"
+
+std::unordered_map<void*, std::pair<int, float>> pAnimatingInfo;
+
+MAKE_HOOK(CBaseAnimating_FrameAdvance, S::CBaseAnimating_FrameAdvance(), float, void* rcx, float flInterval)
+{
+	if (!Vars::Visuals::Removals::Interpolation.Value || rcx == H::Entities.GetLocal())
+		return CALL_ORIGINAL(rcx, flInterval);
+
+	const auto pEntity = static_cast<CBaseEntity*>(rcx);
+
+	if (pEntity && pEntity->IsPlayer())
+	{
+		if (pEntity->m_flSimulationTime() == pEntity->m_flOldSimulationTime() || I::GlobalVars->tickcount == pAnimatingInfo[rcx].first)
+		{
+			pAnimatingInfo[rcx].second += flInterval;
+			return 0.f;
+		}
+	}
+
+	flInterval = pAnimatingInfo[rcx].second; pAnimatingInfo[rcx].second = 0.f; pAnimatingInfo[rcx].first = I::GlobalVars->tickcount;
+	return CALL_ORIGINAL(rcx, flInterval);
+}
